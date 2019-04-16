e9ffc79a19b162654dc5be1d87fb5211e0ccd3df
MikeQVo <angelictide@gmail.com>
2019-04-16 08:05:30 -0600

    working on our pokemon theme

--- src/bouncyDancer.js
+++ src/bouncyDancer.js
@@ -1,6 +1,6 @@
 var BouncyDancer = function(top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, timeBetweenSteps);
-
+  this.$node = $('<div class="pokeball"><div class="pokebar"><div class="pokebutton"><div class="pokebtnMini"></div></div></div></div>');
 
 };
 
@@ -11,6 +11,6 @@ BouncyDancer.prototype.step = function() {
 
   var oldStep = Dancer.prototype.step.bind(this);
   oldStep();
-  this.$node.show().animate({left: ["+=50", "easeInBounce"], opacity: [.25, "swing"]}, {duration: 1000, easing: 'easeOutBounce'});
+  this.$node.show().animate({top: 500 * Math.random(), left: 700 * Math.random() }, {duration: 3000, easing: 'easeOutBounce'});
 
 };
